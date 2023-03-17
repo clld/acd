2a9c38b2bdaf86c7784649e77bbda5a8879b8691
Robert Forkel <xrotwang@googlemail.com>
2023-03-17 10:15:32 +0100

    upgrade clld

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         'unidecode',
         'clldutils>=3.11.1',
-        'clld>=9.2.2',
+        'clld>=10',
         'clldmpg>=4.2',
         'clld-cognacy-plugin',
         'clld-glottologfamily-plugin>=4.0',
